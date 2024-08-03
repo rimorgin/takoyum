559270465693f11f5d56b214679d2a03e8226aba
Sean Calvin Calderon <95837480+rimorgin@users.noreply.github.com>
2024-08-03 22:52:03 +0800

    Update Store.js

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -33,7 +33,7 @@ const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== 'prod',
 });
 
 export const persistor = persistStore(store);
